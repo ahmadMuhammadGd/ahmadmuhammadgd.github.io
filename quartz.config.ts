117adb96acbd21cfa9511f9764d90a29c93a2fa8
ahmadmuhammadgd <ahmadmuhammadgd@gmail.com>
2025-08-09 18:57:53 +0300

    Quartz sync: Aug 9, 2025, 6:57 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "ahmadmuhammad",
+    pageTitle: "AM",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
